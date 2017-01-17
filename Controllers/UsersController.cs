10838a8c1571ddc6fdbf16a7022eeefae4619b94
Miro Dojkic <mdojkic@gmail.com>
2017-01-17 15:24:18 +0100

    Implemented post action for UsersController

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -35,8 +35,9 @@ namespace FesbBoardBackend.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void Post([FromBody]User user)
         {
+            _users.Add(user);
         }
 
         // PUT api/values/5
